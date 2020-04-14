68af10ff5500fba5bf3d7a0ff4c149d2e00fd96f
Giel Goudsmit <giel@goudsmit.tech>
2020-04-14 14:51:38 +0200

    First merger of forked repo and XCode Framework template

---
+++ FullyLoaded/FullyLoaded/FullyLoaded.h
@@ -0,0 +1,19 @@
+//
+//  FullyLoaded.h
+//  FullyLoaded
+//
+//  Created by Giel Goudsmit on 14/04/2020.
+//  Copyright © 2020 Local Heroes Online. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FullyLoaded.
+FOUNDATION_EXPORT double FullyLoadedVersionNumber;
+
+//! Project version string for FullyLoaded.
+FOUNDATION_EXPORT const unsigned char FullyLoadedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FullyLoaded/PublicHeader.h>
+
+
